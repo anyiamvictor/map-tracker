2aa82c96331725621e6045947289c72a1feef4c0
anyiam victor <anyiamvictor@gmail.com>
2025-04-09 08:27:12 +0100

    Adding backend data to test cities

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+import CityList from './components/CityList';
 
 
 function App() {
@@ -17,8 +18,8 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>listsssss</p>} />          
-          <Route path='cities' element={<p>list of cities</p>} />
+          <Route index element={<CityList />} />          
+          <Route path='cities' element={<CityList/>} />
           <Route path='countries' element={<p>list of countries</p>} />
           <Route path='form' element={<p>Input form</p>} />
         </Route>
